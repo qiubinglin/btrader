c1507eb817abf75b835188a23294025c06637c22
qiubinglin <qiubinglin@outlook.com>
2025-04-22 19:38:40 +0800

    opt: Add comment for DataService and TradeService

--- broker/data_service.h
+++ broker/data_service.h
@@ -11,20 +11,83 @@ namespace btra::broker {
 using DataCustomer = journal::Writer;
 
 FORWARD_DECLARE_UPTR(DataService)
+/**
+ * @brief Base class to provide data service.
+ * 
+ */
 class DataService {
 public:
+    /**
+     * @brief Factory function to create different data service according to institution.
+     * 
+     * @param institution Institution name such as binance.
+     * @return DataServiceUPtr 
+     */
     static DataServiceUPtr create(const std::string &institution);
+
+    /**
+     * @brief Set the customer object which enjoy the data service.
+     * 
+     * @param w 
+     */
     void set_customer(DataCustomer *w) { writer_ = w; }
+
+    /**
+     * @brief Setup config.
+     * 
+     * @param cfg 
+     */
     virtual void setup(const Json::json &cfg) = 0;
+
+    /**
+     * @brief Initialize the service?
+     * 
+     */
     virtual void start() = 0;
+
+    /**
+     * @brief Run service.
+     * 
+     */
     virtual void serve() = 0;
 
+    /**
+     * @brief Subscribe instruments.
+     * 
+     * @param instrument_keys 
+     * @return true 
+     * @return false 
+     */
     virtual bool subscribe(const std::vector<InstrumentKey> &instrument_keys) = 0;
+
+    /**
+     * @brief Subscribe all instruments that adapt to the institution.
+     * 
+     * @return true 
+     * @return false 
+     */
     virtual bool subscribe_all() { return false; };
+
+    /**
+     * @brief To do: figure out the usage.
+     * 
+     * @param custom_sub 
+     * @return true 
+     * @return false 
+     */
     virtual bool subscribe_custom(const CustomSubscribe &custom_sub) { return subscribe_all(); };
+
+    /**
+     * @brief Unsubscribe instruments.
+     * 
+     * @param instrument_keys 
+     * @return true 
+     * @return false 
+     */
     virtual bool unsubscribe(const std::vector<InstrumentKey> &instrument_keys) = 0;
 
 protected:
+    /* writer_ will consume the data. */
     DataCustomer *writer_ = nullptr;
 };
 
--- broker/trade_service.h
+++ broker/trade_service.h
@@ -7,14 +7,56 @@
 namespace btra::broker {
 
 FORWARD_DECLARE_UPTR(TradeService)
+/**
+ * @brief Base class to provide trade service.
+ * 
+ */
 class TradeService {
 public:
+    /**
+     * @brief Factory function to create different trade service according to institution.
+     * 
+     * @param institution 
+     * @return TradeServiceUPtr 
+     */
     static TradeServiceUPtr create(const std::string &institution);
+
+    /**
+     * @brief Setup config.
+     * 
+     * @param cfg 
+     */
     virtual void setup(const Json::json &cfg) = 0;
+
+    /**
+     * @brief 
+     * 
+     */
     virtual void start() = 0;
+
+    /**
+     * @brief Get the account type object
+     * 
+     * @return enums::AccountType 
+     */
     [[nodiscard]] virtual enums::AccountType get_account_type() const = 0;
 
+    /**
+     * @brief Insert order.
+     * 
+     * @param input 
+     * @return true 
+     * @return false 
+     */
     virtual bool insert_order(const OrderInput &input) = 0;
+
+    /**
+     * @brief Cancel order.
+     * 
+     * @param input 
+     * @return true 
+     * @return false 
+     */
     virtual bool cancel_order(const OrderAction &input) = 0;
 };
 
