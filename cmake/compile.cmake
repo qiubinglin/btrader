76d082e627649edab804aeea938a2bd82d8db228
qiubinglin <qiubinglin@outlook.com>
2025-07-12 10:47:34 +0800

    build: Set build type = RelWithDebInfo which is `-g -O2`

--- cmake/compile.cmake
+++ cmake/compile.cmake
@@ -1,16 +1,15 @@
 set(CMAKE_C_STANDARD 17)
+set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_C_STANDARD_REQUIRED ON)
 set(CMAKE_C_EXTENSIONS ON) 
-set(CMAKE_CXX_STANDARD 20)
+
+if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE RelWithDebInfo)  # -g -O2
+endif()
+message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC") # set -fPIC for nng
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
-set(CMAKE_C_FLAGS_DEBUG "-g -O0")
-set(CMAKE_CXX_FLAGS_DEBUG "-g -O0")
-
-set(CMAKE_BUILD_TYPE "Debug")
-message("Build type: ${CMAKE_BUILD_TYPE}")
-
 add_compile_options(
     -Wall        # 打开大部分警告
     -Wextra      # 打开额外的警告
