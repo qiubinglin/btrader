ac70d633b3c0ffe29a0a05b46c82b6d6246c52f4
qiubinglin <qiubinglin@outlook.com>
2025-07-26 06:01:54 +0800

    build: Make build type as Debug, while having bug in RelWithDebInfo

--- cmake/compile.cmake
+++ cmake/compile.cmake
@@ -4,7 +4,8 @@ set(CMAKE_C_STANDARD_REQUIRED ON)
 set(CMAKE_C_EXTENSIONS ON) 
 
 if(NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE RelWithDebInfo)  # -g -O2
+    set(CMAKE_BUILD_TYPE Debug)
+    # set(CMAKE_BUILD_TYPE RelWithDebInfo)  # -g -O2
 endif()
 message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
 
