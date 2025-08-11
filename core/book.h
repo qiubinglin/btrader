3ada6acf650c24a33bee587072dca4c9d800cb6f
qiubinglin <qiubinglin@outlook.com>
2025-08-11 20:37:22 +0800

    opt: Add hashid.h

--- core/book.h
+++ core/book.h
@@ -1,19 +1,9 @@
 #pragma once
 
+#include "core/hashid.h"
 #include "core/types.h"
-#include "infra/hash.h"
 
 namespace btra {
-
-inline uint32_t hash_instrument(const infra::Array<char, EXCHANGE_ID_LEN> &exchange_id,
-                                const infra::Array<char, INSTRUMENT_ID_LEN> &instrument_id) {
-    return infra::hash_str_32(instrument_id) ^ infra::hash_str_32(exchange_id);
-}
-
-inline uint32_t hash_product(const infra::Array<char, EXCHANGE_ID_LEN> &exchange_id,
-                             const infra::Array<char, PRODUCT_ID_LEN> &product_id) {
-    return infra::hash_str_32(product_id) ^ infra::hash_str_32(exchange_id);
-}
 
 // key = hash_str_32(product_id) or key = hash_product(exchange_id, product_id)
 class CommissionBook : public std::unordered_map<uint32_t, Commission> {
